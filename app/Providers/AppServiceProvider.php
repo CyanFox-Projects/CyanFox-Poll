12342349cf0c42eef86346966ef807d37d4a00b2
RealZone22 <RealZone22@users.noreply.github.com>
2024-07-29 20:38:20 +0000

    Fix styling

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,7 +20,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(UrlGenerator $url): void
     {
-        if(env('ENFORCE_SSL', false)) {
+        if (env('ENFORCE_SSL', false)) {
             $url->forceScheme('https');
         }
     }
